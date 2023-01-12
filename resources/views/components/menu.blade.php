91fed7266c43ac9fe93ac46be87ba2d897877f6f
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2023-01-12 22:10:47 +0100

    Header: Menu

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,9 +1,26 @@
+<div class="nav contenitore">
+    <nav class="d-flex align-center justify-between">
+        {{-- Logo DC --}}
+        <img src="{{ asset('img/dc-logo.png') }}" alt="">
 
-<nav>
-    <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/fumetti">Fumetti</a></li>
-    </ul>
-</nav>
+        {{-- Menu --}}
+        <ul class="d-flex align-center">
+            <li><a href="/">CHARACTERS</a></li>
+            <li><a href="/fumetti">COMICS</a></li>
+            <li><a href="/fumetti">MOVIE</a></li>
+            <li><a href="/fumetti">TV</a></li>
+            <li><a href="/fumetti">GAMES</a></li>
+            <li><a href="/fumetti">COLLECTIBLES</a></li>
+            <li><a href="/fumetti">VIDEOS</a></li>
+            <li><a href="/fumetti">FANS</a></li>
+            <li><a href="/fumetti">NEWS</a></li>
+            <li><a href="/fumetti">SHOP <span>[]</span></a></li>
+        </ul>
 
-<img src="{{ asset('img/partner.png') }}" alt="">
+        {{-- Searchbar --}}
+        <div class="d-flex align-center">
+            <h4>Search</h4>
+            <span>[]</span>
+        </div>
+    </nav>
+</div>
--- resources/views/components/sites.blade.php
+++ resources/views/components/sites.blade.php
@@ -1,16 +1,18 @@
 {{-- additional DC sites --}}
 
 <div class="sites">
-    <ul>
-        <li>
-            <span>DC POWER</span>
-            <span>[]</span>
-            <span>VISA</span>
-            <span>[]</span>
-        </li>
-        <li>
-            <span>ADDITIONAL DC SITES</span>
-            <span>[]</span>
-        </li>
-    </ul>
+    <div class="contenitore">
+        <ul>
+            <li>
+                <span>DC POWER</span>
+                <span>[]</span>
+                <span>VISA</span>
+                <span>[]</span>
+            </li>
+            <li>
+                <span>ADDITIONAL DC SITES</span>
+                <span>[]</span>
+            </li>
+        </ul>
+    </div>
 </div>
